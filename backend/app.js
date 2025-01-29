59c64e743f3ecc86da3773243a8aab66f3c1e8ec
harshaithireddy <22a91a12a1@aec.edu.in>
2025-01-29 10:08:29 +0530

    Second Commit

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bcrypt = require('bcryptjs');
 
 var FirstRoute = require('./Routes/FirstRoute');
 
